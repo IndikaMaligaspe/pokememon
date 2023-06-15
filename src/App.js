3ded6f182b4c1a1fa094a543235094eb009864f4
indikamaligaspe <k.indika.maligaspe@gmail.com>
2023-06-15 06:59:47 +0530

    added css theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import './App.css';
-// import pokemon from './pokemon.json'
+import "./App.css";
+import styled from "@emotion/styled";
 
+
+const Title = styled.h1`
+  text-align: center;
+`
+
+const TwoColumnLayout = styled.div`
+    display: grid;
+    grid-template-columns: 70% 30%;
+    grid-column-gap: 1rem;
+`
+const Container = styled.div`
+    margin: auto;
+    width: 800px;
+    padding-top: 1rem;
+
+`
+const Input = styled.input`
+  width:100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`
 
 function PokemonInfo( {name, base}) {
   return (
@@ -75,22 +96,11 @@ function App() {
   },[]);
 
   return (
-    <div
-      style={{
-        margin: "auto",
-        width:800,
-        paddingTop:"1rem",
-      }}
-    >
-        <h1 className="title">Pokemon Search</h1>
-
-        <div style={{
-            display:"grid",
-            gridTemplateColumns:"70% 30%",
-            gridColumnGap:"1rem",
-        }}>
+    <Container>
+      <Title>Pokemon Search</Title>
+        <TwoColumnLayout>
           <div>
-          <input name="filter" 
+          <Input name="filter" 
           value={filter} 
           onChange={(evt) => setFilter(evt.target.value)} />
             <table width="100%">
@@ -126,9 +136,8 @@ function App() {
                 )
               }
           </div>
-        </div>  
-        
-    </div>
+      </TwoColumnLayout>  
+    </Container>
   );
 }
 
